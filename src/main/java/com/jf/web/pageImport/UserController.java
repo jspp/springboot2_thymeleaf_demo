3bc86e7526c809347c3792ac308e364e53dc5470
jspp@qq.com <123456>
2019-07-12 18:33:41 +0800

    ajax demo

--- src/main/java/com/jf/web/pageImport/UserController.java
+++ src/main/java/com/jf/web/pageImport/UserController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +31,13 @@ public class UserController {
     public String userInfo02(HttpServletRequest request){
         logger.info(" ★ ★★★ /userInfo02   ★ ★★★  ");
         return "pageImport/userInfo02";
+    }
+
+    @RequestMapping("/userInfoAjax")
+    @ResponseBody
+    public String userInfoAjax(@RequestParam(name = "workCertUrls[]") String[] workCertUrls){
+        logger.info(" ★ ★★★ /userInfoAjax   ★ ★★★ workCertUrls "+workCertUrls[0]);
+        return "success";
     }
 
 }
